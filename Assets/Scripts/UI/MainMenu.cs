36bff34975d227f569340098c0be3955910ab195
Peter18281 <Peter18281@gmail.com>
2024-05-04 14:50:16 +0100

    more main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,18 +6,53 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject tutorial;
-    
-    public void ShowTutorial(){
+    public GameObject lobby;
+    public GameObject joinLobby;
+    public GameObject settings;
+
+    public void ShowTutorial()
+    {
         tutorial.SetActive(true);
     }
 
-    public void HideTutorial(){
+    public void HideTutorial()
+    {
         tutorial.SetActive(false);
+    }
+
+    public void ShowLobby()
+    {
+        lobby.SetActive(true);
+    }
+
+    public void HideLobby()
+    {
+        lobby.SetActive(false);
+    }
+
+    public void ShowJoinLobby()
+    {
+        joinLobby.SetActive(true);
+    }
+
+    public void HideJoinLobby()
+    {
+        joinLobby.SetActive(false);
+    }
+
+    public void ShowSettings()
+    {
+        settings.SetActive(true);
+    }
+
+    public void HideSettings()
+    {
+        settings.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
